a6ab8ed693e73716a93e494374ef135b86865e7e
Nitish2 <nitishn886@gmail.com>
2017-06-12 15:23:57 +0530

    Assignment7.1

---
+++ src/Package1/Car.java
@@ -0,0 +1,11 @@
+package Package1;
+
+public class Car {
+
+	String model= " Swift desire";
+	protected void display()
+	{
+		System.out.println(model);
+	}
+}
+ 
---
+++ src/Package2/Company.java
@@ -0,0 +1,15 @@
+package Package2;
+
+import Package1.Car;
+
+public class Company extends Car {
+
+	public static void main(String[] args) {
+		
+	Company m= new Company();
+		m.display();
+ 
+  
+	}
+ 
+}
